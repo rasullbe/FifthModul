b852f849151bf66e0b5f5cfdf508cfef92818147
rasullbe <rasulbek112208@gmail.com>
2026-04-25 15:53:12 +0500

    Practice

--- Practice/src/PracticeCode/Services/UserService.cs
+++ Practice/src/PracticeCode/Services/UserService.cs
@@ -5,28 +5,78 @@ namespace PracticeCode.Services;
 
 public class UserService : IUserService
 {
+    private static List<User> _users = [];
+
     public Guid CreateUser(UserCreateDto userCreateDto)
     {
-                
+        var userId = Guid.NewGuid();
+        var user = new User
+        {
+            UserId = userId,
+            FirstName = userCreateDto.FirstName,
+            LastName = userCreateDto.LastName,
+            Email = userCreateDto.Email,
+            Password = userCreateDto.Password,
+            PhoneNumber = userCreateDto.PhoneNumber,
+            DateOfBirth = userCreateDto.DateOfBirth
+        };
+
+        _users.Add(user);
+        return userId;
     }
 
     public bool DeleteUserAsync(Guid userId)
     {
-        throw new NotImplementedException();
+        var user = _users.FirstOrDefault(u => u.UserId == userId);
+        if (user == null)
+            return false;
+
+        _users.Remove(user);
+        return true;
     }
 
     public List<UserGetDto> GetAllUsersAsync()
     {
-        throw new NotImplementedException();
+        return _users.Select(u => new UserGetDto
+        {
+            UserId = u.UserId,
+            FirstName = u.FirstName,
+            LastName = u.LastName,
+            Email = u.Email,
+            PhoneNumber = u.PhoneNumber,
+            DateOfBirth = u.DateOfBirth
+        }).ToList();
     }
 
     public UserGetDto? GetUserById(Guid userId)
     {
-        throw new NotImplementedException();
+        var user = _users.FirstOrDefault(u => u.UserId == userId);
+        if (user == null)
+            return null;
+
+        return new UserGetDto
+        {
+            UserId = user.UserId,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            DateOfBirth = user.DateOfBirth
+        };
     }
 
     public bool UpdateUserAsync(Guid userId, UserUpdateDto userUpdateDto)
     {
-        throw new NotImplementedException();
+        var user = _users.FirstOrDefault(u => u.UserId == userId);
+        if (user == null)
+            return false;
+
+        user.FirstName = userUpdateDto.FirstName;
+        user.LastName = userUpdateDto.LastName;
+        user.Email = userUpdateDto.Email;
+        user.PhoneNumber = userUpdateDto.PhoneNumber;
+        user.DateOfBirth = userUpdateDto.DateOfBirth;
+
+        return true;
     }
 }
